bc9c78a1da2314b21ba19d13a51d2864ea632422
hydhyro <133247743+hydhyro@users.noreply.github.com>
2026-08-23 21:29:20 -0400

    Add files via upload

---
+++ lang/engmoves_dialogue.lua
@@ -0,0 +1,105 @@
+-- Script text
+-- Keyed by the original text label. The English is in the comment.
+
+--to do: 
+--You should take a quick nap
+--It will whisk you\nback --safron pidgey
+--enfermeira silphco9 - 3text
+--push(game, ("%s got a\nMAGIKARP!"):format(game.save.player.name), done)
+
+--YELLOW stuff:
+--["OAK: Look, {PLAYER}! Do\nyou see that ball\non the table?"] = "CARVALHO: Olhe,\n{PLAYER}!\fVê essa bola ali\nna mesa?",
+return {
+  ["_CeruleanCityCooltrainerF1SlowbroPunchText"] = "SLOWBRO, POUND!\n...Errou de novo!",
+  ["_CeruleanCityCooltrainerF1SlowbroUseSonicboomText"] = "OK! SLOWBRO! Use\no SONICBOOM!\vQualé, SLOWBRO,\vpreste atenção!",
+  ["_CeruleanCityCooltrainerF1SlowbroWithdrawText"] = "SLOWBRO, WITHDRAW!\nNão! Está errado!\fÉ tão difícil\ncontrolar alguns\vPOKéMON!\fA obediência dos\nPOKéMON depende\vda sua habilidade\vcomo treinador!",
+
+
+  ["_CeruleanBadgeHouseBoulderBadgeText"] = "O ATAQUE de todos\nPOKéMON aumentará\vum pouco.\fTambém te permite\nusar FLASH em\vqualquer momento.",
+  ["_CeruleanBadgeHouseCascadeBadgeText"] = "POKéMON até o {SLOT:BB}30\nvão te obedecer.\fUm pouco mais, e\nficarão rebeldes!\fTambém te permite\nusar CUT fora\vdas batalhas.",
+  ["_CeruleanBadgeHouseRainbowBadgeText"] = "POKéMON até o {SLOT:BB}50\nvão te obedecer.\fUm pouco mais, e\nficarão rebeldes!\fTambém te permite\nusar STRENGTH fo-\vra das batalhas.",
+  ["_CeruleanBadgeHouseSoulBadgeText"] = "A DEFESA de todos\nPOKéMON aumentará\vum pouco.\fTambém te permite\nusar SURF fora\vdas batalhas.",
+  ["_CeruleanBadgeHouseThunderBadgeText"] = "A VELOCIDADE de\ntodos os POKéMON\vsubirá um pouco.\fTambém te permite\nusar FLY fora\vdas batalhas.",
+  ["_CannotUseTeleportNowText"] = "{RAM:wNameBuffer} não\npode usar o\vTELEPORT agora!",
+
+
+  ["_CeruleanTrashedHouseFishingGuruTheyStoleATMText"] = "Esses malditos\nROCKETs!\fOlha o que\nfizeram aqui!\fRoubaram minha MT\nque ensina DIG\vaos POKéMON pra\vCAVAR buracos!\fIsso me deu um\nbaita prejuízo!",
+  ["_CeruleanTrashedHouseFishingGuruWhatsLostIsLostText"] = "O que está perdido\nestá perdido!\fEu decidi que vou\nensinar DIG ao\vDIGLETT sem a MT!",
+  ["_CeruleanTrashedHouseGirlText"] = "A EQUIPE ROCKET\ndeve estar que-\vrendo o DIG para\ventrar em alguma\vfalcatrua!",
+  ["_FuchsiaGymKogaPostBattleAdviceText"] = "Quando são afeta-\ndos pelo TOXIC,\vPOKéMON sofrerão\vcada vez mais\vconforme a luta\vprogride!\fIsso com certeza\naterrorizará os\vadversários!",
+  ["_Route12Gate2FBrunetteGirlTM39ExplanationText"] = "MT39 contém a\ntécnica chamada\vSWIFT.\fÉ bem preciso,\nentão use nas\vbatalhas que não\vpode perder.",
+  ["_CeladonCityGramps3TM41ExplanationText"] = "MT41 ensina a\nSOFTBOILED!\fApenas um POKéMON\npode usar isso!\fEsse POKéMON se\nchama CHANSEY!",
+  ["_ViridianCityFisherTM42ExplanationText"] = "MT42 contém \nDREAM EATER...\v...Ronc...",
+  ["_SilphCo2FSilphWorkerFTM36ExplanationText"] = "MT36 contém\nAUTODESTRUIÇÃO!\fÉ poderosa, mas o\nPOKéMON que usar\vessa técnica irá\vdesmaiar!\vSeja cauteloso.",
+  ["_CinnabarQuizQuestionsText6"] = "TM28 contém\nROCK TOMB?",
+  ["_CopycatsHouse2FCopycatTM31Explanation1Text"] = "\fMT31 contém meu\ngolpe favorito,\vMIMIC!\fUse isso em um\nbom POKéMON!",
+  ["_MrPsychicsHouseMrPsychicTM29ExplanationText"] = "MT29 contém\nPSYCHIC!\fEsse golpe pode\nabaixar o atribu-\vto de ESPECIAL do\vseu adversário.",
+  ["_CeladonMart3FClerkTM18ExplanationText"] = "MT18 ensina o\nCOUNTER!\vEle causa o dobro\vde dano que o seu\vPOKéMON receber!",
+  ["_CeladonMartRoofLittleGirlTM13ExplanationText"] = "\f{RAM:wStringBuffer} contém\nICE BEAM!\fPode congelar o\nalvo às vezes!",
+  ["_CeladonMartRoofLittleGirlTM48ExplanationText"] = "\f{RAM:wStringBuffer} contém\nROCK SLIDE!\fPode assustar o\nalvo às vezes!",
+  ["_CeladonMartRoofLittleGirlTM49ExplanationText"] = "\fMT49 contém\nTRI-ATTTACK!",
+
+ --GYM
+  ["_TM34ExplanationText"] = "\fUma MT contém uma\ntécnica que pode\vser ensinada para\vum POKéMON!\fAs MT só funcionam\numa vez! Então\vao ensinar uma\vnova técnica,\vescolha o POKéMON\vcom cuidado!\fMT34 contém\nBIDE!\fSeu POKéMON irá\nabsorver o dano\vrecebido durante\va batalha e então\vdevolver em dobro!",
+  ["_CeruleanGymMistyTM11ExplanationText"] = "MT11 ensina\nBUBBLEBEAM!\fEnsine para um\nPOKéMON aquático!",
+  ["_VermilionGymLTSurgeThunderBadgeInfoText"] = "A INSÍGNIA TROVÃO\nirá aumentar a\vVELOCIDADE dos\vseus POKéMON!\fTambém permite que\nseus POKéMON usem\va técnica FLY a\vqualquer momento!\fVocê é especial,\ngaroto! Fique\vcom isso também!",
+  ["_TM24ExplanationText"] = "\fTM24 contém\nTHUNDERBOLT!\fEnsine para um\nPOKéMON elétrico!",
+  ["_TM21ExplanationText"] = "\fMT21 contém\nMEGA DRAIN.\fMetade do dano\ncausado é drenado\ve irá curar o seu\vPOKéMON!",
+  ["_TM46ExplanationText"] = "\fMT46 contém\nPSY WAVE!\vEssa técnica usa\vpoderosas ondas\vde energia psí-\vquica para causar\vdano!",
+  ["_FuchsiaGymKogaTM06ExplanationText"] = "\fMT06 contém\nTOXIC!\fÉ uma técnica\nsecreta de mais\vde 400 anos!",
+  ["_CinnabarGymBlainePostBattleAdviceText"] = "FIRE BLAST é\na técnica suprema\vdo tipo fogo!\fNão desperdice ela\nusando contra\vPOKéMON de água!",
+  ["_CinnabarGymBlaineTM38ExplanationText"] = "\fMT38 contém\nFIRE BLAST!\fEnsine para um\nPOKéMON de fogo!\fCHARMELEON ou\nPONYTA seriam\vboas escolhas!",
+  ["_ViridianGymGiovanniTM27ExplanationText"] = "\fMT27 contém \nFISSURE!\vIsso derrota um\vPOKéMON com ape-\vnas um golpe!\fEu a criei quando\neu ainda cuidava\vdesse GINÁSIO, há\vmuito tempo...",
+
+
+
+ --BATALHA
+  ["_BuildingRageText"] = "{USER}\naumentou sua RAGE!",
+  ["_MirrorMoveFailedText"] = "MIRROR MOVE\nfalhou!",
+  ["_TooWeakSubstituteText"] = "Sem forças para\nusar SUBSTITUTE!",
+  ["_SubstituteBrokeText"] = "O SUBSTITUTE\nse quebrou!",
+  ["_SubstituteText"] = "Um SUBSTITUTE\nfoi criado!",
+  ["_SubstituteTookDamageText"] = "O SUBSTITUTE tomou\no dano no lugar\vde {TARGET}!",
+  ["_HasSubstituteText"] = "{USER} já tem\num SUBSTITUTE!",
+ 
+ 
+ --HM01
+  ["_CeruleanGymMistyCascadeBadgeInfoText"] = "A INSÍGNIA CASCATA\nfará todos os\vPOKéMON até o {SLOT:BB}30\vte obedecerem!\fIsso inclui até\nos forasteiros,\vque você recebeu\vcom trocas!\fE tem mais, você\npoderá usar a\vtécnica CUT a\vqualquer hora!\fPoderá cortar os\narbustos e abrir\vnovos caminhos!\fVocê também pode\nficar com a minha\vMT predileta!",
+  ["_Route2TradeHouseScientistText"] = "Um POKéMON que\ndesmaiou não pode\vbatalhar. Mas\vainda pode usar\vuma das técnicas\vespeciais, como\va do CUT!",
+  ["_SSAnne2FRivalCutMasterText"] = "{RIVAL}: Ouvi\ndizer que havia\vum mestre na téc-\vnica de CUT\va bordo.\fMas ele é só um\nvelhote enjoado!\fMas CUT é algo\nrealmente útil!\fVocê deveria ir\nver esse velhote!\fFalou aí, tô indo\nnessa!",
+  ["_SSAnne2FRoomsGrampsText"] = "Um POKéMON pode\nusar CUT em\vpequenos arbustos.",
+  ["_SSAnne3FSailorText"] = "Nosso CAPITÃO é um\ngrande mestre\vespadachim!\fEle até ensina a\ntécnica CUT\vaos POKéMON!",
+  ["_SSAnneCaptainsRoomCaptainIFeelMuchBetterText"] = "CAPITÃO: Ufa!\nObrigado! Já me\vsinto bem melhor!\fVocê quer ver\nminha técnica\vde CUT?\fEu poderia te\nmostrar se não\vestivesse mal...\fJá sei! Você pode\nficar com isso!\fEnsine isso para\num POKéMON e\vuse CUT a\vqualquer hora!",
+  ["_UsedCutText"] = "{RAM:wNameBuffer} usou\nCUT!",
+  ["_ViridianForestNorthGateGrampsText"] = "Você reparou nos\narbustos ao longo\vda rota?\fVocê pode cortar\neles usando uma\vtécnica especial\vde um POKéMON.",
+  ["_NothingToCutText"] = "Não tem nada para\nusar CUT aqui!",
+ --HM02  
+  ["_Route16FlyHouseBrunetteGirlHM02ExplanationText"] = "MO02 contém FLY.\nCom isso você po-\vderá voltar para\vqualquer cidade.\fFaça bom uso\ndisso!",
+  ["_CannotFlyHereText"] = "{RAM:wNameBuffer} não po-\nde usar FLY aqui.",
+  ["_CeruleanBadgeHouseThunderBadgeText"] = "A VELOCIDADE de\ntodos os POKéMON\vsubirá um pouco.\fTambém te permite\nusar FLY fora\vdas batalhas.",
+  ["_PokemonFanClubExplainBikeVoucherText"] = "\fTroque isso por\numa BICICLETA!\fNão se preocupe,\nmeu FEAROW sabe\vFLY e me leva\va qualquer lugar!\fEntão, não preciso\nde uma BICICLETA!\fEspero que goste\nde pedalar!",
+  ["_Route13CooltrainerM2AfterBattleText"] = "Fui derrotado. Eu\nacho que vou usar\vFLY e ir pra casa.",
+  ["_Route14CooltrainerM4BattleText"] = "Já ensinou seu\nPOKéMON pássaro a\vtécnica FLY?",
+  ["_Route20CooltrainerMAfterBattleText"] = "Agora meu pássaro\nnão pode usar FLY!",
+  ["_VermilionGymLTSurgeThunderBadgeInfoText"] = "A INSÍGNIA TROVÃO\nirá aumentar a\vVELOCIDADE dos\vseus POKéMON!\fTambém permite que\nseus POKéMON usem\va técnica FLY a\vqualquer momento!\fVocê é especial,\ngaroto! Fique\vcom isso também!",   
+ --HM03
+  ["_SafariZoneSecretHouseFishingGuruHM03ExplanationText"] = "MO03 contém\nSURF!\fOs POKéMON serão\ncapazes de te \vtransportar atra-\vvés da água!\fE essa MO não\né descartável!\vVocê pode usar\vvárias vezes!\fVocê é realmente\nsortudo por ter\vganho um prêmio\vtão fabuloso!",
+  ["_CyclingIsFunText"] =   "Pedalar é mais\nlegal que o SURF!",
+  ["_FuchsiaGymKogaSoulBadgeInfoText"] = "Agora que você tem\na INSÍGNIA ALMA,\va DEFESA de todos\vos seus POKéMON\virá aumentar!\fEla também permite\nque você use a MO\vSURF fora das\vbatalhas!\fAh! Fique com\nisso também!",
+  ["_Route20Swimmer1AfterBattleText"] = "Eu também queria\npoder usar SURF\vem meus POKéMON!",
+ --HM04
+   ["_WardensHouseWardenHM04ExplanationText"] = "DIRETOR: A MO04\ncontém STRENGTH!\fIsso permite que\nseu POKéMON possa\vmover grandes\vrochas do seu\vcaminho fora das\vbatalhas.\fAh, sim, você já\nencontrou a CASA\vSECRETA da ZONA\vDO SAFARI?\fSe você achar,\nganhará uma MO!\fOuvi dizer que é a\nrara MO do SURF.",
+   ["_BoulderText"] = "Precisa usar\nSTRENGTH para\vmover isso!",
+   ["_SSAnneB1FRoomsSuperNerdText"] = "Meu parceiro,\nMACHOKE, é super\vforte!\fEle tem uma super\nSTRENGTH capaz de\vempurrar até\vpedras gigantes!",
+   ["_UsedStrengthText"] = "{RAM:wNameBuffer} usou\nSTRENGTH.",
+   ["_CeladonGymRainbowBadgeInfoText"] = "A INSÍGNIA\nARCO-ÍRIS vai\vfazer todos os\vPOKéMON até o {SLOT:BB}50\vte obedecerem.\fTambém permitirá\nque seus POKéMON\vusem a técnica\voculta STRENGTH\vdentro e fora das\vbatalhas.\fPor favor, também\nfique com isso.",
+ --HM05  
+  ["_DexRatingText_Own10To19"] = "Você está no\ncaminho certo! \vPegue a MO FLASH\vcom meu ajudante\vna ROTA 2!",
+  ["_DiglettsCaveRoute2FishingGuruText"] = "Eu fui até o TÚNEL\nROCHOSO, mas é\vmuito escuro e\ve assustador.\fSe um POKéMON\nusar a técnica de\vCLARÃO isso iria\viluminar tudo...",
+  ["_FlashLightsAreaText"] = "Um FLASH brilhoso\niluminou a área!",
+  ["_PewterGymBrockBoulderBadgeInfoText"] = "\fÉ uma INSÍGNIA\noficial da LIGA\vPOKéMON!\fSeus POKéMON\nficarão mais\vpoderosos!\fA técnica FLASH\nagora pode ser\vusada a qualquer\vmomento!",
+  ["_Route2GateOaksAideFlashExplanationText"] = "A MO FLASH\nilumina até mesmo\va mais escura\vdas cavernas.",
+  ["_Route2GateYoungsterText"] = "Quando um POKéMON\naprender FLASH,\vvocê poderá atra-\vvessar o TÚNEL\vROCHOSO.",
+  ["_DexRatingText_Own10To19"] = "Você está no\ncaminho certo! \vPegue a MO FLASH\vcom meu ajudante\vna ROTA 2!",
+
+}
---
+++ lang/engmoves_strings.lua
@@ -0,0 +1,22 @@
+--strings para nomes de golpes em inglês...
+return {
+ 
+  ["PSN"] = "PSN",
+  ["FNT"] = "FNT",
+  ["SLP"] = "SLP",
+  ["FRZ"] = "FRZ",
+  ["BRN"] = "BRN",
+  
+  ["%s\nused %s!"] = "%s\nusou %s!",
+  
+  ["FLY"] = "FLY",
+  ["FLASH"] = "FLASH",
+  ["CUT"] = "CUT",
+  ["SURF"] = "SURF",
+  ["STRENGTH"] = "STRENGTH",
+  ["SOFTBOILED"] = "SOFTBOILED",
+  ["TELEPORT"] = "TELEPORT",
+  ["DIG"] = "DIG",
+  
+}
+
